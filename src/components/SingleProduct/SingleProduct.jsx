5af44c45f9195eaf55cac8f9a43904cd0914ac7c
Akshar Vijay <aksharvij@gmail.com>
2023-07-02 20:56:06 +0530

    add and remove functionality

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import styles from "./SingleProduct.module.css";
 import { Card, Button } from 'react-bootstrap';
 import Rating from '../Rating/Rating';
+import { CartState } from '../../context/Context';
 
 const SingleProduct = ({product}) => {
+
+    const {state: {cart}, dispatch} = CartState()
+
+    const AddToCartHandler = () => {
+        dispatch(
+            {
+                type: "ADD_TO_CART",
+                payload: product
+            }
+        )
+    }
+    const RemoveFromCartHandler = () => {
+        dispatch(
+            {
+                type: "REMOVE_FROM_CART",
+                payload: product
+            }
+        )
+    }
   return (
     <div className={styles.products}>
     <Card>
@@ -21,10 +41,15 @@ const SingleProduct = ({product}) => {
             }
             <Rating rating={product.ratings}/>
         </Card.Subtitle>
-        <Button variant='danger'> Remove from Cart</Button>
-        <Button disabled={product.inStock}>
+        { cart.some((p) => p.id === product.id) ? (
+            <Button variant='danger'onClick={RemoveFromCartHandler}> Remove from Cart</Button>
+        ):(
+            <Button onClick={AddToCartHandler}disabled={product.inStock}>
             {!product.inStock ? "Add to Cart" : "Out of Stock"}
-        </Button>
+            </Button>
+        )}
+        
+
       </Card.Body>
     </Card>
     </div>
